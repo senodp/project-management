7031dbef189f838e6528e80733f5c69d9d0dd429
Seno Dwi Prasetyo <senothoha77@gmail.com>
2025-11-18 13:24:28 +0700

    .

--- models/board.go
+++ models/board.go
@@ -11,7 +11,7 @@ type Board struct {
 	PublicID   		uuid.UUID `json:"public_id" db:"public_id"`
 	Title 				string `json:"title" db:"title"`
 	Description 	string `json:"description" db:"description"`
-	OwnerID 			int64 `json: "owner_internal_id" db="owner_internal_id" gorm:"colomn:owner_internal_id"`
+	OwnerID 			int64 `json:"owner_internal_id" db="owner_internal_id" gorm:"column:owner_internal_id"`
 	OwnerPublicID uuid.UUID `json:"owner_public_id" db:"owner_public_id"`
 	CreatedAt 		time.Time `json:"created_at" db:"created_at"`
 	DueDate 			*time.Time `json:"due_date,omitempty" db:"due_date"`
