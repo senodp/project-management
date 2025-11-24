b0d6598913912b5223da688ca167182099182814
Seno Dwi Prasetyo <senothoha77@gmail.com>
2025-11-24 09:50:27 +0700

    create list repository #2
    Create function Update, Delete, Update position, Get card position

--- repositories/list_repository.go
+++ repositories/list_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/google/uuid"
 	"github.com/senodp/project-management/config"
 	"github.com/senodp/project-management/models"
 )
@@ -17,4 +18,28 @@ func NewListRepository() ListRepository {
 
 func (r *listRepository) Create(list *models.List) error {
 	return config.DB.Create(list).Error
+}
+
+func (r *listRepository) Update(list *models.List) error {
+	return config.DB.Model(&models.List{}).Where("public_id = ?",list.PublicID).
+	Updates(map[string]interface{}{
+		"title": list.Title,
+	}).Error
+}
+
+func (r *listRepository) Delete(id uint) error {
+	return config.DB.Delete(&models.List{},id).Error
+}
+
+func (r *listRepository) UpdatePosition(boardPublicID string, position []string) error {
+	return config.DB.Model(&models.ListPosition{}).
+	Where("board_internal_id = (Select internal_id FROM boards Where public_id = ?)",boardPublicID).
+	Update("list_order", position).Error
+}
+
+func (r *listRepository) GetCardPosition(listPublicID string) ([]uuid.UUID, error) {
+	var position models.CardPosition
+	err :=  config.DB.Joins("JOIN lists ON list.internal_id = card_positions.list_internal_id").
+	Where("list.public_id = ?", listPublicID).Error
+	return position.CardOrder,err
 }
